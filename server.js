c59421a0edb6c771d1f2c1a41e863c39132d3c38
Thai Phan <thaiphan90@yahoo.com>
2019-08-24 00:33:23 -0500

    trying to make htmlRoutes work

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 var app = express();
-
 var PORT = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
     res.send("Hellow World")
 })
 
+//Body parser stuff
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-app.use(bodyParser.urlencoded({extended: false}))
+//Needed for routes to work
+app.use(express.static('app/public'));
 
-app.use(bodyParser.json())
+//Router
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
 
-app.use(function (req, res) {
-    res.setHeader('Content-type', 'text/plain')
-    res.write('you posted:\n')
-    res.end(JSON.stringify(req.body, null, 2))
-})
-
-
+//Displays port beingl listend on
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
 });
