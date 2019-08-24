6a79fa1ae249ada395fbf881d60f453bcf6d2804
Thai Phan <thaiphan90@yahoo.com>
2019-08-24 00:44:41 -0500

    made survey router work

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -14,7 +15,7 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(express.static('app/public'));
 
 //Router
-require('./app/routing/apiRoutes.js')(app);
+//require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
 //Displays port beingl listend on
