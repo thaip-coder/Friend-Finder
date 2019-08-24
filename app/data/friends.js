691f8e31fd10c8d1fd02865a820f2f94ed559965
Thai Phan <thaiphan90@yahoo.com>
2019-08-23 23:35:54 -0500

    added friends to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        "name": "Gengar",
+        "photo": "pkmn/gengar.png",
+        "scores": [2, 1, 5, 4, 3, 5, 1, 2, 1, 4]
+    },
+    {
+        "name": "Milotic",
+        "photo": "pkmn/milotic.png",
+        "scores": [4, 3, 2, 1, 2, 2, 1, 1, 2, 1]
+    },
+    {
+        "name": "Metagross",
+        "photo": "pkmn/metagross.png",
+        "scores": [4, 3, 2, 5, 1, 1, 4, 2, 5, 1]
+    },
+    {
+        "name": "Typhlosion",
+        "photo": "pkmn/typhlosion.png",
+        "scores": [1, 5, 5, 3, 2, 2, 5, 4, 2, 5]
+    },
+    {
+        "name": "Tyranitar",
+        "photo": "pkmn/tyranitar.png",
+        "scores": [2, 2, 4, 1, 3, 5, 2, 4, 4, 3]
+    },
+    {
+        "name": "Gardevoir",
+        "photo": "pkmn/gardevoir.png",
+        "scores": [1, 3, 1, 3, 4, 4, 3, 2, 2, 3]
+    }
+]
+
+module.exports = friends;
