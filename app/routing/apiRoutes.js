262c3253ba3b571050c81cf0a038d6dfbc16192e
Thai Phan <thaiphan90@yahoo.com>
2019-08-24 15:19:20 -0500

    starting up api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,22 @@
 //The api routes are for our data.
 //Helps determine what data the user sees, as well as what data the user is able to post to our server to store.
 
+var friends = require('..data/friends.js');
 
+//Routing
+module.exports = function(app) {
+
+    //API GET request
+    app.get('/api/friends', function(req, res) {
+        res.json(friends)
+    });
+
+    //API POST request
+    app.post('/api/friends', function(req, res) {
+
+    });
+
+    
+}
 
 
