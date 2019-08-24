b614deb08684b85c492930ed3a290e3f42ee39c3
Thai Phan <thaiphan90@yahoo.com>
2019-08-24 00:59:18 -0500

    updated

--- server.js
+++ server.js
@@ -8,11 +8,6 @@ var PORT = process.env.PORT || 3000;
 //Body parser stuff
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
-
-//Needed for routes to work
-app.use(express.static('app/public'));
 
 //Router
 //require('./app/routing/apiRoutes.js')(app);
