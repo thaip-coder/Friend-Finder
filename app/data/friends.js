db134aeae44102c9063839b371a39733ed78ef0d
Thai Phan <thaiphan90@yahoo.com>
2019-08-24 19:27:26 -0500

    fix pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,32 @@
 var friends = [
     {
         "name": "Gengar",
-        "photo": "/pkmn/gengar.png",
+        "picture": "/pkmn/gengar.png",
         "scores": [2, 1, 5, 4, 3, 5, 1, 2, 1, 4]
     },
     {
         "name": "Milotic",
-        "photo": "/pkmn/milotic.png",
+        "picture": "/pkmn/milotic.png",
         "scores": [4, 3, 2, 1, 2, 2, 1, 1, 2, 1]
     },
     {
         "name": "Metagross",
-        "photo": "/pkmn/metagross.png",
+        "picture": "/pkmn/metagross.png",
         "scores": [4, 3, 2, 5, 1, 1, 4, 2, 5, 1]
     },
     {
         "name": "Typhlosion",
-        "photo": "/pkmn/typhlosion.png",
+        "picture": "/pkmn/typhlosion.png",
         "scores": [1, 5, 5, 3, 2, 2, 5, 4, 2, 5]
     },
     {
         "name": "Tyranitar",
-        "photo": "/pkmn/tyranitar.png",
+        "picture": "/pkmn/tyranitar.png",
         "scores": [2, 2, 4, 1, 3, 5, 2, 4, 4, 3]
     },
     {
         "name": "Gardevoir",
-        "photo": "/pkmn/gardevoir.png",
+        "picture": "/pkmn/gardevoir.png",
         "scores": [1, 3, 1, 3, 4, 4, 3, 2, 2, 3]
     }
 ]
