052c7195b73333b38a1085f15a55b914a5c114b8
Thai Phan <thaiphan90@yahoo.com>
2019-08-25 09:54:31 -0500

    updated friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -28,7 +28,28 @@ var friends = [
         "name": "Gardevoir",
         "picture": "https://cdn.bulbagarden.net/upload/thumb/9/99/282Gardevoir.png/1200px-282Gardevoir.png",
         "scores": [1, 3, 1, 3, 4, 4, 3, 2, 2, 3]
+    },
+    {
+        "name": "Ash Ketchum",
+        "picture": "http://media.pocketmonsters.net/characters/2.png",
+        "scores": [3, 4, 4, 4, 4, 3, 1, 2, 5, 1]
+    },
+    {
+        "name": "Brock Harrison",
+        "picture": "https://animesuperhero.com/fansites/blogicon/52642/master/1455331486.jpg",
+        "scores": [1, 2, 5, 3, 3, 4, 1, 2, 4, 3]
+    },
+    {
+        "name": "Misty Yawa",
+        "picture": "http://pm1.narvii.com/6372/e5cdf8f2c401c02d2652f5ae684d9f35ce14048e_00.jpg",
+        "scores": [3, 3, 4, 1, 2, 5, 5, 5, 1, 3]
+    },
+    {
+        "name": "Professor Oak",
+        "picture": "https://thehardtimes.net/wp-content/uploads/2019/02/professor-oak.jpg",
+        "scores": [4, 5, 5, 2, 5, 4, 5, 4, 2, 5]
     }
+
 ]
 
 module.exports = friends;
