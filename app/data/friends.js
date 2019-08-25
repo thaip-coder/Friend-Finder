bc2e7aa885996b55a36a6b72b76f9faf77f0a94f
Thai Phan <thaiphan90@yahoo.com>
2019-08-24 20:34:34 -0500

    linked pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
     {
         "name": "Gengar",
-        "picture": "./pkmn/gengar.png",
+        "picture": "https://cdn.bulbagarden.net/upload/thumb/c/c6/094Gengar.png/500px-094Gengar.png",
         "scores": [2, 1, 5, 4, 3, 5, 1, 2, 1, 4]
     },
     {
         "name": "Milotic",
-        "picture": "./pkmn/milotic.png",
+        "picture": "https://cdn.bulbagarden.net/upload/thumb/3/36/350Milotic.png/500px-350Milotic.png",
         "scores": [4, 3, 2, 1, 2, 2, 1, 1, 2, 1]
     },
     {
         "name": "Metagross",
-        "picture": "./pkmn/metagross.png",
+        "picture": "https://cdn.bulbagarden.net/upload/thumb/0/05/376Metagross.png/500px-376Metagross.png",
         "scores": [4, 3, 2, 5, 1, 1, 4, 2, 5, 1]
     },
     {
         "name": "Typhlosion",
-        "picture": "./pkmn/typhlosion.png",
+        "picture": "https://cdn.bulbagarden.net/upload/thumb/4/47/157Typhlosion.png/500px-157Typhlosion.png",
         "scores": [1, 5, 5, 3, 2, 2, 5, 4, 2, 5]
     },
     {
         "name": "Tyranitar",
-        "picture": "./pkmn/tyranitar.png",
+        "picture": "https://cdn.bulbagarden.net/upload/thumb/8/82/248Tyranitar.png/1200px-248Tyranitar.png",
         "scores": [2, 2, 4, 1, 3, 5, 2, 4, 4, 3]
     },
     {
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,16 +18,16 @@ module.exports = function(app) {
         var pkmnMatch = 0;
         var userArray = parseInt(userScore);
         
-
         //For loop that runs through all pokemon and calculates scores for each one according to user input
         for(var i = 0; i < friends.length; i++) {
             var scoreDifference = 0;
             for(var j = 0; j < userArray.length; j++) {
-                scoreDifference += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(userArray[j])));
+                scoreDifference += (Math.abs(friends[i].scores[j] - userArray[j]));
             }
             //Pushes all results to scoreArray
             scoreArray.push(scoreDifference);
         }
+        console.log(scoreArray);
 
         //Finds best match after comparing pokemon scores to user input
         for(var i = 0; i < scoreArray.length; i++) {
@@ -38,7 +38,7 @@ module.exports = function(app) {
 
         //Assigns best matched pokemon
         var pokemon = friends[pkmnMatch];
-        res.json(pokemon);                      
+        res.json(pokemon);                     
 
     });  
 
